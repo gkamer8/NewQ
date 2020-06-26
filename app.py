c9e119349c90e4c60d4bd19f059a1b30ed7ddad3
Gordon <gkamer@outlook.com>
2020-06-26 16:31:47 -0400

    added templates

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'Here\'s looking at you, kid!'
 
 
 if __name__ == '__main__':
